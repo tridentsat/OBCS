50fd3adef46dd3b4775e45e87606c5f710fb8fb0
upendra <upendra1999kadre@gmail.com>
2020-03-09 19:33:08 +0530

    all up to date

--- git_init_commit.sh
+++
@@ -1,7 +0,0 @@
-#! /bin/bash
-read -p "Enter git repository name: " repo
-git init
-git add .
-git commit -m "Initial commit"
-git remote add origin https://github.com/tridentsat/$repo.git
-git push -u origin master
--- gitcommit.sh
+++
@@ -1,6 +0,0 @@
-#! /bin/bash
-read -p "Enter git commit messege : " messege
-git add .
-echo "$messege"
-git commit -m "$messege"
-git push -u origin master
