76fac59cf0f87f8e5c33087cf1b16d14858c761d
upendra <upendra1999kadre@gmail.com>
2020-03-09 17:49:29 +0530

    Organised

--- gitcommit.sh
+++ gitcommit.sh
@@ -1,7 +1,7 @@
 #! /bin/bash
-read -p "Enter git repository name: " repo
 read -p "Enter git commit messege : " messege
 git add .
+echo "$messege"
 git commit -m "$messege"
 git remote add origin https://github.com/tridentsat/OBCS.git
 git push -u origin master
