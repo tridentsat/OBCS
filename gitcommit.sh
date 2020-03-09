6164e57fe1d3179e80fac08a02b567fcb29b3a7e
tridentsat <trident.satlab@gmail.com>
2020-03-09 14:59:27 +0530

    better scrpits

--- gitcommit.sh
+++ gitcommit.sh
@@ -3,5 +3,5 @@ read -p "Enter git repository name: " repo
 read -p "Enter git commit messege : " messege
 git add .
 git commit -m "$messege"
-git remote add origin https://github.com/tridentsat/$repo.git
+git remote add origin https://github.com/tridentsat/OBCS.git
 git push -u origin master
